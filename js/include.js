5986037a6cc9031f21fc88a5d98c034f95410cf0
박지유 <jiyeupak@gmail.com>
2026-05-27 10:52:19 +0900

    플랜투 글자 변경

--- js/include.js
+++ js/include.js
@@ -1,19 +1,21 @@
-
-document.write('<title>오브젠</title>');
+document.write("<title>플랜투 비즈니스 컨설팅</title>");
 
 // favicon
 document.write('<link rel="icon" href="/img/obzen_favicon.png"></link>');
 
 // obzen Tag Manager
-    var _mtm = window._mtm = window._mtm || [];
-    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://dxlab.obzen.com/TagManager/obzen_AvHKmUMI.js';
-    s.parentNode.insertBefore(g,s);
-    
-    // var _mtm = window._mtm = window._mtm || [];
-    // _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-    // var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    // g.async=true; g.src='https://demo.obzen.com/TagManager/obzen_w2ONxBtH.js';
-    //     s.parentNode.insertBefore(g,s);
+var _mtm = (window._mtm = window._mtm || []);
+_mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+var d = document,
+  g = d.createElement("script"),
+  s = d.getElementsByTagName("script")[0];
+g.async = true;
+g.src = "https://dxlab.obzen.com/TagManager/obzen_AvHKmUMI.js";
+s.parentNode.insertBefore(g, s);
+
+// var _mtm = window._mtm = window._mtm || [];
+// _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+// var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+// g.async=true; g.src='https://demo.obzen.com/TagManager/obzen_w2ONxBtH.js';
+//     s.parentNode.insertBefore(g,s);
 // End obzen Tag Manager
